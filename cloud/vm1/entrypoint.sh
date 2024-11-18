8b5137aac612720dd612f9dfe1d7f35f7c04c862
Mehedi <me69oshan@gmail.com>
2024-11-19 00:51:24 +0600

    db traffic via proxy server

--- cloud/vm1/entrypoint.sh
+++ cloud/vm1/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Replace environment variables in the template file
-envsubst '${ES_UPSTREAM} ${KAFKA_UPSTREAM} ${REDIS_UPSTREAM} ${ES_PORT} ${KAFKA_PORT} ${REDIS_PORT}' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+envsubst '${ES_UPSTREAM} ${KAFKA_UPSTREAM} ${REDIS_UPSTREAM} ${DB_UPSTREAM} ${DB_DIRECT_UPSTREAM} ${ES_PORT} ${KAFKA_PORT} ${REDIS_PORT} ${DB_PORT} ${DB_DIRECT_PORT}' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
 
 # Start Nginx
 nginx -g "daemon off;"
